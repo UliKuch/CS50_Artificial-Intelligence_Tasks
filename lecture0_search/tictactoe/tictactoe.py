128e1647fe2546ddfabdaa133ac8e867a6a8d934
UliKuch <u.kuchelmeister@gmx.de>
2020-05-22 15:45:02 +0200

    Add alpha-beta pruning to tictactoe; refactor & add comments

--- lecture0_search/tictactoe/tictactoe.py
+++ lecture0_search/tictactoe/tictactoe.py
@@ -97,28 +97,36 @@ def minimax(board):
     """
     if terminal(board):
         return None
+
     possible_actions = actions(board)
     current_player = player(board)
     action_value_pairs = set()
 
+    # random action on empty board
     if board == initial_state():
         return random.choice(tuple(possible_actions))
 
+    # calculate values for possible actions
     for action in possible_actions:
         resulting_board = result(board, action)
+        # immediately returns if action leads to victory
+        # (also kind of alpha-beta pruning, I think)
         if (terminal(resulting_board)) and (winner(resulting_board) == current_player):
             return action
-        action_value_pairs.add((action, calc_value(resulting_board)))
+        action_value_pairs.add((action, value(resulting_board)))
 
-    if current_player == X:
-        best_result = max(pair[1] for pair in action_value_pairs)
-    else:
-        best_result = min(pair[1] for pair in action_value_pairs)
+    # calculate best result depending on who is current player
+    action_values = [pair[1] for pair in action_value_pairs]
+    best_result_possible = best_result(action_values, current_player)
 
-    return [pair[0] for pair in action_value_pairs if pair[1] == best_result][0]
+    # return first action achieving best result
+    return [pair[0] for pair in action_value_pairs if pair[1] == best_result_possible][0]
 
 
-def calc_value(board):
+def value(board):
+    """
+    Returns the value of a given board.
+    """
     possible_actions = actions(board)
     current_player = player(board)
     action_values = set()
@@ -126,13 +134,29 @@ def calc_value(board):
     if terminal(board):
         return utility(board)
 
+    # caluculate values for all possible actions
     for action in possible_actions:
         resulting_board = result(board, action)
         if terminal(resulting_board):
-            action_values.add(utility(resulting_board))
+            if winner(resulting_board) == current_player:
+                # alpha-beta pruning: if result is best possible result
+                # for current player (i.e. winning), immediately return it
+                return utility(resulting_board)
+            else:
+                # add value of current action to list
+                action_values.add(utility(resulting_board))
         else:
-            action_values.add(calc_value(resulting_board))
+            # recursively call self if board is not terminal
+            action_values.add(value(resulting_board))
 
+    # return best possible value (if opponent plays perfectly) for current player
+    return best_result(action_values, current_player)
+
+
+def best_result(action_values, current_player):
+    """
+    Returns the best result for the current player out of a list of action values.
+    """
     if current_player == X:
         return max(action_values)
     else:
